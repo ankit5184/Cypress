3108738242e240d8fa4bf3bd649369121588e1aa
ankit5184 <Anki@t5184>
2022-10-23 22:47:50 +0530

    commit

---
+++ cypress/support/commands.js
@@ -0,0 +1,22 @@
+import LoginPage from "../Pages/LoginPage"
+import LogoutPage from "../Pages/LogoutPage"
+
+const loginPage = new LoginPage()
+const logoutPage = new LogoutPage()
+const name = "standard_user"
+const password = "secret_sauce"
+
+
+
+Cypress.Commands.add('login', () => {
+    loginPage.navigateToLogin(name, password)
+})
+Cypress.Commands.add('logout', () => {
+    logoutPage.Logout()
+})
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+})
